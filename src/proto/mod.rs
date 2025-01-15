1f8f4903d888193db197b9bffef4280826aa9107
samsond <nosmaspoise@gmail.com>
2025-01-14 17:12:16 -0800

    Temporarily comment out duplicate 'connect' function to resolve build error

---
+++ src/proto/mod.rs
@@ -0,0 +1,23 @@
+pub mod common {
+    include!("milvus.proto.common.rs");
+}
+
+pub mod feder {
+    include!("milvus.proto.feder.rs");
+}
+
+pub mod milvus {
+    include!("milvus.proto.milvus.rs");
+}
+
+pub mod msg {
+    include!("milvus.proto.msg.rs");
+}
+
+pub mod rg {
+    include!("milvus.proto.rg.rs");
+}
+
+pub mod schema {
+    include!("milvus.proto.schema.rs");
+}
